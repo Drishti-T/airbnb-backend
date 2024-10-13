1478674279ca38f76914dee1f898034d485b6660
root <root@LAPTOP-7B54OGNU>
2024-10-13 19:10:55 +0530

    added v1 folder and routes

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+//importing v1 route
+const v1 = require('./v1/index');
+
+app.use('/api/v1', v1);
+
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`)
+})
