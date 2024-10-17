1e85e5c39edeca903f119dfcb3a486523ab74a67
root <root@LAPTOP-7B54OGNU>
2024-10-17 12:46:37 +0530

    restored all changes

--- src/v1/index.js
+++ src/v1/index.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 //importing router
 const userRoute = require('./routes/user');
-const adminRoute = require('./routes/admin');
+// const adminRoute = require('./routes/admin');
+
 
 router.use('/user', userRoute);
-router.use('/admin' , adminRoute);
+// router.use('/admin' , adminRoute);
 
 module.exports = router;
